c83c796664abf4bd4ad3a7f62dded2fe33048e60
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-03-07 08:53:38 +0900

    Initial Commit

---
+++ Myjyanken/MyjyankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyjyankenApp.swift
+//  Myjyanken
+//
+//  Created by administrator on 2021/03/07.
+//
+
+import SwiftUI
+
+@main
+struct MyjyankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
